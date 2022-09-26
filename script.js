cfe81606135486aa17d5586020c09ae5d602b4b9
Sabas Segovia <sasegovia@ucdavis.edu>
2022-09-25 23:46:24 -0700

    feat: sprite animations using trig

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = 'enemy2.png';
+    this.image.src = 'enemy3.png';
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 266;
-    this.spriteHeight = 188;
+    this.spriteWidth = 218;
+    this.spriteHeight = 177;
     this.width = this.spriteWidth/2;
     this.height = this.spriteHeight / 2;
     this.x = Math.random() * (canvas.width - this.width);
@@ -23,12 +23,12 @@ class Enemy {
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     this.angle = 0 //Math.random() * 2;
-    this.angleSpeed = Math.random() * .2;
+    this.angleSpeed = Math.random() * .5 + .5;
   }
   update() {
-    this.x -= this.speed;
+    this.x = (canvas.width/2) * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
+    this.y = (canvas.height/2) * Math.cos(this.angle * Math.PI/700) + (canvas.height/2 - this.height/2);
     if (this.x + this.width < 0) this.x = canvas.width;
-    this.y += 3 * Math.sin(this.angle);
     this.angle += this.angleSpeed;
     //animate sprites
     if (gameFrame % this.flapSpeed === 0) {
