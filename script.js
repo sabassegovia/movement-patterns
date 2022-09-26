6d4f475bffcb4e2d2ff94ce364a1f2f95c48294e
Sabas Segovia <sasegovia@ucdavis.edu>
2022-09-25 23:31:10 -0700

    feat: adding sine wave movement to characters

--- script.js
+++ script.js
@@ -3,23 +3,40 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
-const numEnemies = 100;
+const numEnemies = 20;
 const enemiesArr = [];
+
+let gameFrame = 0;
+
 
 class Enemy {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height = 100;
-    this.speed = Math.random() * 4 - 2;
+    this.image = new Image();
+    this.image.src = 'enemy2.png';
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
+    this.width = this.spriteWidth/2;
+    this.height = this.spriteHeight / 2;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.angle = 0 //Math.random() * 2;
+    this.angleSpeed = Math.random() * .2;
   }
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
+    this.x -= this.speed;
+    if (this.x + this.width < 0) this.x = canvas.width;
+    this.y += 3 * Math.sin(this.angle);
+    this.angle += this.angleSpeed;
+    //animate sprites
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? this.frame = 0 : this.frame++;
+    }
   }
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height)
+    ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height,);
   }
 }
 
@@ -33,6 +50,7 @@ function animate() {
     enemy.draw()
     enemy.update();
   })
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
