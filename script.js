6e1b2855fd0e60aba28a9682363e64f25e65a039
Sabas Segovia <sasegovia@ucdavis.edu>
2022-09-25 23:51:52 -0700

    feat: starting collision detection

--- script.js
+++ script.js
@@ -12,24 +12,32 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = 'enemy3.png';
+    this.image.src = 'enemy4.png';
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 218;
-    this.spriteHeight = 177;
+    this.spriteWidth = 213;
+    this.spriteHeight = 213;
     this.width = this.spriteWidth/2;
     this.height = this.spriteHeight / 2;
     this.x = Math.random() * (canvas.width - this.width);
     this.y = Math.random() * (canvas.height - this.height);
+    this.newX = Math.random() * canvas.width;
+    this.newY = Math.random() * canvas.height;
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-    this.angle = 0 //Math.random() * 2;
-    this.angleSpeed = Math.random() * .5 + .5;
+    this.interval = Math.floor( Math.random() * 200 + 50);
   }
   update() {
-    this.x = (canvas.width/2) * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
-    this.y = (canvas.height/2) * Math.cos(this.angle * Math.PI/700) + (canvas.height/2 - this.height/2);
+    if (gameFrame % this.interval === 0) {
+      this.newX = Math.random() * (canvas.width - this.width);
+      this.newY = Math.random() * (canvas.height - this.height);
+    }
+    let dx = this.x - this.newX;
+    let dy = this.y - this.newY;
+    this.x -= dx/30;
+    this.y -= dy/30;
+
+
     if (this.x + this.width < 0) this.x = canvas.width;
-    this.angle += this.angleSpeed;
     //animate sprites
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? this.frame = 0 : this.frame++;
